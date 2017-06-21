d68de285e360d010eeb702132a0636acccab2a71
dmcIntyre1515 <DarrinMcIntyre1515@gmail.com>
2017-06-21 12:01:45 -0400

    Round and User pojos, some DAO interface methods added

--- src/main/java/chat/chickentalk/dao/Dao.java
+++ src/main/java/chat/chickentalk/dao/Dao.java
@@ -1,9 +1,23 @@
 package chat.chickentalk.dao;
 
+import java.util.ArrayList;
+
 import chat.chickentalk.pojos.Person;
+import chat.chickentalk.pojos.Round;
+import chat.chickentalk.pojos.User;
 
 public interface Dao {
 
-    Person getPersonById(int id);
-
+	public Person getPersonById(int id); // testing
+	
+	// User Methods
+    User getUserById(int id);
+    ArrayList<User> getAllUsers();
+    
+    // Round Methods
+    Round getRoundById(int id);
+    ArrayList<Round> getAllRounds();
+    
+    // Join Methods
+    
 }
