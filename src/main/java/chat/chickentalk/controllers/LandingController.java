79afbb8a56e93288c030281d0bbdd826881c5e2a
Richard Marquez <richard92m@me.com>
2017-06-23 19:45:03 -0400

    MVChicken

---
+++ src/main/java/chat/chickentalk/controllers/LandingController.java
@@ -0,0 +1,16 @@
+package chat.chickentalk.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LandingController {
+
+    @RequestMapping(value = "/landing", method = RequestMethod.GET)
+    public String getLanding() {
+        System.out.println("GET landing");
+        return "landing";
+    }
+
+}
